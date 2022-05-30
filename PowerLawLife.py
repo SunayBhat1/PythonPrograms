96b08673ad284852419e856d76ae3c6e6e8ded2a
SunayBhat1 <56206141+SunayBhat1@users.noreply.github.com>
2022-05-30 13:23:34 -0700

    Power law updates

--- PowerLawLife.py
+++ PowerLawLife.py
@@ -1,15 +1,20 @@
+# https://betterprogramming.pub/how-to-send-push-notifications-to-your-phone-from-any-script-6b70e34748f6
+
+
 from urllib import request
 import numpy as np
 
-key = "Lmdwc3Ei4h0vfiIwLA4K0"
+key_Sunay = "Lmdwc3Ei4h0vfiIwLA4K0"
+key_Matthew = "nHy_ScCt8nLNGZhaprZ9LSGQEV4CaNr8XSPmEzKdRU7"
 
 
 # Fasting/Diet
-if np.random.pareto(1.16,1)>75:
+if np.random.pareto(1.16,1)>50:
     message = 'Full_Fast_Day!!!'
-elif np.random.pareto(1.16,1)>15:
+elif np.random.pareto(1.16,1)>12:
     message = 'Dinner_Only_Day!!'
 else:
     message = 'Normal_Day!'
 
-request.urlopen("https://maker.ifttt.com/trigger/notify/with/key/%s?value1=%s" % (key,message));
+request.urlopen("https://maker.ifttt.com/trigger/notify/with/key/%s?value1=%s" % (key_Sunay,message));
+request.urlopen("https://maker.ifttt.com/trigger/notify/with/key/%s?value1=%s" % (key_Matthew,message));
